72d924baa8480a9a65ae8d648143c7bbe45e5dbc
UBC Student <woxtoby@students.cs.ubc.ca>
2022-11-29 13:22:40 -0800

    import font awesome, first three icons

--- my-app/src/interests.js
+++ my-app/src/interests.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+    faHtml5,
+    faCss3,
+    faReact
+} from '@fortawesome/free-brands-svg-icons'
+import '/Users/willoxtoby/Documents/GitHub/personal-portfolio/my-app/src/styles/interests.css';
+
+
 const Interests = () => {
     return (
         <div>
             <h3>My Skills</h3>
             <div>
-                <img/>
-                <img/>
-                <img/>
-                <img/>
-                <img/>
-                <img/>
+            <FontAwesomeIcon icon={faHtml5} size="3x" className='skillIcon'/>
+            <FontAwesomeIcon icon={faCss3}  size="3x" className='skillIcon'/>
+            <FontAwesomeIcon icon={faReact} size="3x" className='skillIcon'/>
             </div>
         </div>
     );
