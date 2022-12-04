dee9add104d687e074246d744b67ec452ee5c093
UBC Student <woxtoby@students.cs.ubc.ca>
2022-12-04 00:25:27 -0800

    added links for interests

--- my-app/src/interests.js
+++ my-app/src/interests.js
@@ -14,9 +14,15 @@ const Interests = () => {
         <div>
             <h3>My Skills</h3>
             <div>
-            <FontAwesomeIcon icon={faHtml5} size="3x" className='skillIcon'/>
-            <FontAwesomeIcon icon={faCss3}  size="3x" className='skillIcon'/>
-            <FontAwesomeIcon icon={faReact} size="3x" className='skillIcon'/>
+                <a href="https://www.w3schools.com/html/" target="_blank">
+                    <FontAwesomeIcon icon={faHtml5} size="3x" className='skillIcon'/>
+                </a>
+                <a href="https://www.w3.org/Style/CSS/Overview.en.html" target="_blank">
+                    <FontAwesomeIcon icon={faCss3}  size="3x" className='skillIcon'/>
+                </a>
+                <a href="https://reactjs.org/" target="_blank">
+                    <FontAwesomeIcon icon={faReact} size="3x" className='skillIcon'/>
+                </a>
             </div>
         </div>
     );
