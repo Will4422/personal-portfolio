c75d7bdd525e60ca492a3cc6a82b7b947f87e4a7
UBC Student <woxtoby@students.cs.ubc.ca>
2022-12-04 21:33:31 -0800

    styling, links

--- my-app/src/navigation/navbar.js
+++ my-app/src/navigation/navbar.js
@@ -1,11 +1,14 @@
 import React from "react";
-
-import bootstrap from 'bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCode} from "@fortawesome/free-solid-svg-icons"
+import "/Users/willoxtoby/Documents/GitHub/personal-portfolio/my-app/src/styles/navbar.css"
 
 const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#" className="navBrand">
+                <FontAwesomeIcon icon={faCode} size="2x"/>
+            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
